166cd71f94254fcc57dc1c4f96fe17421e4dad68
Readone <android-armv7a-belalang-tempur@users.noreply.github.com>
2016-02-01 17:35:41 +0700

    initial OMNI bringup

--- omni.dependencies.mk
+++ omni.dependencies.mk
@@ -1,21 +1,26 @@
 [
-  {
+  { "remote": "MI",
     "repository": "android_kernel_xiaomi_ferrari",
     "target_path": "kernel/xiaomi/ferrari"
+    "revision": "android-6.0"
   },
-  {
+  { "remote": "MI",
     "repository": "android_device_qcom_common",
     "target_path": "device/qcom/common"
+    "revision": "android-6.0"
   },
-  {
+  { "remote": "MI",
     "repository": "android_external_stlport",
     "target_path": "external/stlport"
+    "revision": "android-6.0"
   },
-  {
+  { "remote": "MI",
     "repository": "android_external_sony_boringssl-compat",
     "target_path": "external/sony/boringssl-compat"
+    "revision": "android-6.0"
   },
-  {
+  { "remote": "MI",
     "repository": "android_external_connectivity",
     "target_path": "external/connectivity"
+    "revision": "android-6.0"
   }
